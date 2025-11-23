925ef1a8f2347bdbc2b4cc382977893be5324c4c
Guilherme Pereira Barbosa <guilhermepereira4237@gmail.com>
2025-11-23 17:33:29 -0300

    Logica de CSharp

---
+++ C#/entendendo a logica de CSharp/Saida_dados/Saida_dados/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+
+namespace saida_dados;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Bom dia");
+        Console.Write("Boa noite");
+
+        Console.WriteLine();
+        
+        Console.WriteLine("Bom dia");
+        Console.WriteLine("Boa noite");
+
+        Console.WriteLine();
+
+        int x, y;
+        x = 10;
+        y = 20;
+        Console.WriteLine(x);
+        Console.WriteLine(y);
+
+        Console.WriteLine();
+
+        CultureInfo CI = CultureInfo.InvariantCulture;
+        double x1;
+        x1 = 2.3456;
+        Console.Write(x1.ToString("F2", CI));
+
+
+        Console.WriteLine();
+        Console.WriteLine();
+
+        int idade;
+        double salario;
+        string nome;
+        char sexo;
+        
+        idade = 32;
+        salario = 4560.9;
+        nome = "Maria Silva";
+        sexo = 'F';
+
+        Console.WriteLine("A funcionaria " + nome + ", sexo "
+         + sexo + " , ganha  " + salario.ToString("F2", CI) +
+        " e tem " + idade + " anos");
+
+
+    }
+}
---
+++ C#/entendendo a logica de CSharp/processamento-e-casting/processamento-e-casting/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            x = 5;
+            y = 2;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+            Console.WriteLine();
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            int x1;
+            double y1;
+            x1 = 5;
+            y1 = 2 * x;
+            Console.WriteLine(x1);
+            Console.WriteLine(y1.ToString("F1", CI));
+
+            Console.WriteLine();
+
+            double b1, b2, h, area;
+            b1 = 6.0;
+            b2 = 8.0;
+            h = 5.0;
+            area = (b1 + b2) / 2.0 * h;
+            Console.WriteLine(area);
+
+            Console.WriteLine();
+
+            int a, b, resultado;
+            a = 5;
+            b = 2;
+            resultado = a / b;
+            Console.WriteLine(resultado);
+
+            Console.WriteLine();
+
+            double aa;
+            int bb;
+            aa = 5.0;
+            bb = (int)aa;
+            Console.WriteLine(bb);
+
+        }
+    }
+}
